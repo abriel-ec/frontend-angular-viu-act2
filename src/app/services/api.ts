e753a8a6f6478102e0bed7eeae91d8b1990decea
Diego <diegogabriel.manchenobarba@gmail.com>
2026-02-20 00:23:22 -0500

    Update

---
+++ src/app/services/api.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api {
+  private url = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  get(endpoint: string): Observable<any> {
+    return this.http.get(`${this.url}/${endpoint}`);
+  }
+
+  post(endpoint: string, data: any): Observable<any> {
+    return this.http.post(`${this.url}/${endpoint}`, data);
+  }
+
+  put(endpoint: string, data: any): Observable<any> {
+    return this.http.put(`${this.url}/${endpoint}`, data);
+  }
+
+  delete(endpoint: string): Observable<any> {
+    return this.http.delete(`${this.url}/${endpoint}`);
+  }
+}
